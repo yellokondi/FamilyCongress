1a6d6be2d173e2d38ce808db5f1e6842f5991bb4
Konrad Makowski <yellokondi@yahoo.com>
2015-09-18 14:33:13 -0500

    A few fixes, mainly for sponsors/cooperators info.

--- App_Start/DBInitiatorConfig.cs
+++ App_Start/DBInitiatorConfig.cs
@@ -3,16 +3,17 @@ using FamilyCongress2015.Models;
 
 namespace FamilyCongress2015
 {
-    public class DBInitiatorConfig
-    {
-        public static void InitializeDatabase()
-        {
-            SqlFuDAL.CreateTable<Participant>();
-            SqlFuDAL.CreateTable<Article>();
-            SqlFuDAL.CreateTable<Blog>();
-            SqlFuDAL.CreateTable<Tag>();
-            SqlFuDAL.CreateBlogTagsLinkerTable();
-            SqlFuDAL.CreateTable<Conference>();
-        }
-    }
+	public class DBInitiatorConfig
+	{
+		public static void InitializeDatabase()
+		{
+			SqlFuDAL.CreateTable<Participant>();
+			SqlFuDAL.CreateTable<Article>();
+			SqlFuDAL.CreateTable<Blog>();
+			SqlFuDAL.CreateTable<Tag>();
+			SqlFuDAL.CreateBlogTagsLinkerTable();
+			SqlFuDAL.CreateTable<Conference>();
+			SqlFuDAL.CreateTable<Cooperator>();
+		}
+	}
 }
